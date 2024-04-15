d7addc8eac5cc7f3bb5b695e299a62ae7962488a
Usuario <Usuario@CPD03-M05>
2024-04-15 12:04:53 -0300

    Adicionar arquivos de projeto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace custoViagem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace custoViagem
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
